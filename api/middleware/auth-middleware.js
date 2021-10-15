40ee77713687bd6e47362050a80f3e1bea28280f
TraNequaFauntleroy <shecodes@nenethecreative.com>
2021-10-15 18:50:51 -0400

    wip

--- api/middleware/auth-middleware.js
+++ api/middleware/auth-middleware.js
@@ -4,7 +4,7 @@ const checkUsernameExists = async (req, res, next) => {
       try {
         const [user] = await findBy({username: req.body.username})
         if (!user) {
-          next({status: 401, message: 'invalid credentials'})
+          next({status: 401, message: 'username taken'})
         } else {
           req.user = user
           next()
